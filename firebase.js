dd3b6b2dc51cbbd73bfafa9088b296e65f888876
Muhammad Imran <44212121+muimran@users.noreply.github.com>
2025-06-09 23:31:58 +0600

    cdn firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDwiENl4UYBXVU2Hs-UR8X0J3QOYXP4fro",
-    authDomain: "vocabapp-105be.firebaseapp.com",
-    projectId: "vocabapp-105be",
-    storageBucket: "vocabapp-105be.firebasestorage.app",
-    messagingSenderId: "510549747096",
-    appId: "1:510549747096:web:fce965c86242ad21b7b7ed"
-  };
+  apiKey: "AIzaSyDwiENl4UYBXVU2Hs-UR8X0J3QOYXP4fro",
+  authDomain: "vocabapp-105be.firebaseapp.com",
+  projectId: "vocabapp-105be",
+  storageBucket: "vocabapp-105be.firebasestorage.app",
+  messagingSenderId: "510549747096",
+  appId: "1:510549747096:web:fce965c86242ad21b7b7ed"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
